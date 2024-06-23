cf1bac8c42b6a44bbe5a316306f67bb8a567cdd4
warscodev <43365178+warscodev@users.noreply.github.com>
2024-06-23 22:52:27 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.26 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i=my_string.length(); i>0; i--){
+            answer += my_string.substring(i-1,i);
+        }
+        
+        return answer;
+    }
+}
